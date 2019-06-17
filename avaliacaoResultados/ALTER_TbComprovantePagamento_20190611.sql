33a9e2d1ecfd47e9d2216eeb3d3b579fe7a14f99
wouerner <wouerner@protonmail.com>
2019-06-17 11:45:21 -0300

    add

---
+++ avaliacaoResultados/ALTER_TbComprovantePagamento_20190611.sql
@@ -0,0 +1,4 @@
+ALTER TABLE BDCorporativo.scSAC.tbComprovantePagamento ALTER COLUMN idArquivo int NULL
+ALTER TABLE BDCorporativo.scSAC.tbComprovantePagamento ADD nrCodigoAcesso VARCHAR(200) NULL
+ALTER TABLE BDCorporativo.scSAC.tbComprovantePagamento ADD nrProdutoNota VARCHAR(200) NULL  
+ 
