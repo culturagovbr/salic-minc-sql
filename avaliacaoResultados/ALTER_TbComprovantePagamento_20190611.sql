54a9440904e6131a2be629f2d560a5800614b0cf
wouerner <wouerner@protonmail.com>
2019-06-18 14:24:35 -0300

    add script de rollback

--- avaliacaoResultados/ALTER_TbComprovantePagamento_20190611.sql
+++ avaliacaoResultados/ALTER_TbComprovantePagamento_20190611.sql
@@ -1,4 +1,10 @@
+
+-- Script para execução 
 ALTER TABLE BDCorporativo.scSAC.tbComprovantePagamento ALTER COLUMN idArquivo int NULL
 ALTER TABLE BDCorporativo.scSAC.tbComprovantePagamento ADD nrCodigoAcesso VARCHAR(200) NULL
 ALTER TABLE BDCorporativo.scSAC.tbComprovantePagamento ADD nrProdutoNota VARCHAR(200) NULL  
  
+-- Script para defazer a execução
+ALTER TABLE BDCorporativo.scSAC.tbComprovantePagamento drop column nrProdutoNota 
+ALTER TABLE BDCorporativo.scSAC.tbComprovantePagamento drop column nrCodigoAcesso 
+ALTER TABLE BDCorporativo.scSAC.tbComprovantePagamento ALTER COLUMN idArquivo int NOT NULL
